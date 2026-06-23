3db9c34949b48d777d14a4097f31e1eda3551dcc
Marcus Edmark <maredm@kth.se>
2026-06-23 12:15:27 +0000

    Fix faulty typescript addition function in math.js module

--- old/static/modules/math.js
+++ old/static/modules/math.js
@@ -1,17 +1,17 @@
-export function logspace(start: number, end: number, num: number): number[] {
+export function logspace(start, end, num) {
     const logStart = Math.log10(start);
     const logEnd = Math.log10(end);
     const logStep = (logEnd - logStart) / (num - 1);
     return Array.from({ length: num }, (_, i) => Math.pow(10, logStart + i * logStep));
 }
 
-export function linspace(start: number, end: number, num: number): number[] {
+export function linspace(start, end, num) {
     if (num === 1) return [start];
     const step = (end - start) / (num - 1);
     return Array.from({ length: num }, (_, i) => start + i * step);
 }
 
-export function closest(num: number, arr: number[]): number {
+export function closest(num, arr) {
     let curr = arr[0];
     let diff = Math.abs(num - curr);
     let index = 0;
@@ -26,10 +26,10 @@ export function closest(num: number, arr: number[]): number {
     return index;
 }
 
-export function clamp(v: number, lower: number, upper: number): number {
+export function clamp(v, lower, upper) {
     return Math.max(lower, Math.min(upper, v));
 }
 
-export const average = (array: number[]): number => array.reduce((a, b) => a + b) / array.length;
+export const average = (array) => array.reduce((a, b) => a + b) / array.length;
 
-export const abs = (re: number, im: number = 0): number => Math.sqrt(re * re + im * im);
+export const abs = (re, im = 0) => Math.sqrt(re * re + im * im);
